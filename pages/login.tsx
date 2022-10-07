5c125e0e72efa817bb3e72e93e1faaa7c52ee0b2
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2022-10-07 12:41:30 -0300

    react

---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header/Header';
+
+export default function LoginPage () {
+  return (
+    <>
+    <Header/>
+    <h1>Bem Vindo Banco Digital</h1>
+    </>
+  )
+}
+
+
+//tudo no react tem a ver com componetização
+//so posso retornar um componente e não dois
